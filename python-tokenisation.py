c6752466a0743e8c7d3deb3a4b72e73bd455118a
Khaled <codepower.help@gmail.com>
2025-03-17 18:04:10 +0100

    Update to use function and preprocess the text

--- python-tokenisation.py
+++ python-tokenisation.py
@@ -3,10 +3,22 @@ import nltk
 nltk.download("punkt")
 nltk.download("punkt_tab")
 
-text = "The sun dipped below the horizon, casting a warm golden glow over the ocean. A gentle breeze rustled through the trees, carrying the scent of salt and sand. As the stars began to twinkle, the night felt peaceful and full of possibilities."
+text = [
+    "The sun dipped below the horizon,",
+    "casting a warm golden glow over the ocean.",
+    "A gentle breeze rustled through the trees, ",
+    "carrying the scent of salt and sand. ",
+    "As the stars began to twinkle, the night felt peaceful and full of possibilities.",
+]
 
-word_tokens = nltk.word_tokenize(text)
-print("Word Tokens : ", word_tokens)
 
-sentence_tokens = nltk.sent_tokenize(text)
-print("Sentence Tokens : ", sentence_tokens)
+def preprocessing(text):
+    tokens = nltk.word_tokenize(text.lower())
+    return [word for word in tokens if word.isalnum()]
+
+
+preprocesses_doc = [" ".join(preprocessing(doc)) for doc in text]
+print(preprocesses_doc)
+
+# sentence_tokens = nltk.sent_tokenize(text)
+# print("Sentence Tokens : ", sentence_tokens)
